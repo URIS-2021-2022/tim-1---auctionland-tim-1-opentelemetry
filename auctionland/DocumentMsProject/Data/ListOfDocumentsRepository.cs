39ce5e77d1ee73b65e5ca24c41ab50f939fd279c
Zeljana-Amidzic <zeljana.amidzic@uns.ac.rs>
2022-02-17 17:19:31 +0100

    Document mikroservis izmene

---
+++ auctionland/DocumentMsProject/Data/ListOfDocumentsRepository.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using DocumentMsProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentMsProject.Data
+{
+    public class ListOfDocumentsRepository : IListOfDocumentsRepository
+    {
+        private readonly IMapper mapper;
+        private readonly DocumentMsContext context;
+
+        public ListOfDocumentsRepository(IMapper mapper, DocumentMsContext context)
+        {
+            this.mapper = mapper;
+            this.context = context;
+        }
+
+        public ListOfDocumentsConfirmation CreateListOfDocuments(ListOfDocuments listOfDocuments)
+        {
+            var created = context.Add(listOfDocuments);
+            return mapper.Map<ListOfDocumentsConfirmation>(created.Entity);
+        }
+
+        public void DeleteListOfDocuments(Guid listID)
+        {
+            context.ListOfDocuments.Remove(GetListOfDocumentsById(listID));
+        }
+
+        public List<ListOfDocuments> GetAllListOfDocuments()
+        {
+            return context.ListOfDocuments.ToList();
+        }
+
+        public ListOfDocuments GetListOfDocumentsById(Guid listID)
+        {
+            return context.ListOfDocuments.FirstOrDefault(e => e.ListOfDocumentsId.Equals(listID));
+        }
+
+        public bool SaveChanges()
+        {
+            return context.SaveChanges() > 0;
+        }
+
+        public void UpdateListOfDocuments(ListOfDocuments listOfDocuments)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ auctionland/DocumentMsProject/Entities/ListOfDocuments.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentMsProject.Entities
+{
+    public class ListOfDocuments
+    {
+        /// <summary>
+        /// ID liste dokumenata
+        /// </summary>
+        [Key]
+        public Guid ListOfDocumentsId { get; set; }
+
+        public DateTime ListCreationDate { get; set; }
+    }
+}
