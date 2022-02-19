c0f522183e46b227e53cb866bbdd8a9235dcb046
DjoricMilic <djoric.it8.2018@uns.ac.rs>
2022-02-19 20:19:33 +0100

    tehnicki dug 19.02.

--- AddressMicroservice/AddressMicroservice/Program.cs
+++ AddressMicroservice/AddressMicroservice/Program.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace AddressMicroservice
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
