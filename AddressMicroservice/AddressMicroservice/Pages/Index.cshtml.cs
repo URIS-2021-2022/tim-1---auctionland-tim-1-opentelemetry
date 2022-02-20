328275df4d00698057a821ad08bf00d951c14949
DjoricMilic <djoric.it8.2018@uns.ac.rs>
2022-02-20 19:16:04 +0100

    tehnicki dug 20.02.

--- AddressMicroservice/AddressMicroservice/Pages/Index.cshtml.cs
+++ AddressMicroservice/AddressMicroservice/Pages/Index.cshtml.cs
@@ -10,16 +10,15 @@ namespace AddressMicroservice.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel()
         {
-            _logger = logger;
+ 
         }
 
         public void OnGet()
         {
-
+            //Nije potrebna implementacija
         }
     }
 }
