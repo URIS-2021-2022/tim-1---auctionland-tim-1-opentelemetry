911351825de22f61596ca3310daab7340d92ab16
Zeljana-Amidzic <zeljana.amidzic@uns.ac.rs>
2022-02-20 20:41:21 +0100

    RegistracijaMS

---
+++ PublicBiddingRegistrationMicroservice/PublicBiddingRegistrationMicroservice/Models/ApplicationConformationDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PublicBiddingRegistrationMicroservice.Models
+{
+    public class ApplicationConformationDto
+    {
+        public Guid ApplicationId { get; set; }
+
+        #region Payment
+        public Guid PaymentId { get; set; }
+        #endregion
+
+        #region Buyer
+        public Guid BuyerId { get; set; }
+        #endregion
+    }
+}
