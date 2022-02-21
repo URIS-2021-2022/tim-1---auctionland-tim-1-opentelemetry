853adcc8c32678f7ec8ee8e78827ca2e7265336e
gasicmilica <m.gasic.99@gmail.com>
2022-02-22 00:36:51 +0100

    Gateway, komunikacija zalbe sa loggerom

---
+++ ComplaintMicroservice/ComplaintMicroservice/ServiceCalls/PublicBiddingService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ComplaintMicroservice.Models;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace ComplaintMicroservice.ServiceCalls
+{
+    public class PublicBiddingService : IPublicBiddingService
+    {
+        private readonly IConfiguration configuration;
+
+        public PublicBiddingService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public bool GetPublicBidding(PublicBiddingDto publicBiddingDto)
+        {
+            using HttpClient client = new();
+            var x = configuration["Services:PublicBiddingService"];
+            Uri url = new($"{ configuration["Services:PublicBiddingService"] }api/publicBiddings");
+
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(publicBiddingDto));
+            content.Headers.ContentType.MediaType = "application/json";
+
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
