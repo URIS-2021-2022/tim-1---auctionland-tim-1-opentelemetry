7c7ea9b4d449ea72953978d8b3528990ef41279f
DjoricMilic <djoric.it8.2018@uns.ac.rs>
2022-02-22 21:35:47 +0100

    22.02. tehnicki dug AddressMicroservice

--- AddressMicroservice/AddressMicroservice/Controllers/AuthenticationController.cs
+++ AddressMicroservice/AddressMicroservice/Controllers/AuthenticationController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace AddressMicroservice.Controllers
 {
@@ -25,6 +26,9 @@ namespace AddressMicroservice.Controllers
         /// <param name="principal">Model sa podacima na osnovu kojih se vrši autentifikacija</param>
         /// <returns></returns>
         [HttpPost("authenticate")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesDefaultResponseType]
         public IActionResult Authenticate(Principal principal)
         {
             //Pokušaj autentifikacije
