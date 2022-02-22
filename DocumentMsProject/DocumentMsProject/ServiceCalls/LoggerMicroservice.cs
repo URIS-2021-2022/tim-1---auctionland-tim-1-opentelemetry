0accd2a8ab4258d8ed67324300de1a29f5cd252c
Zeljana-Amidzic <zeljana.amidzic@uns.ac.rs>
2022-02-22 23:16:24 +0100

    tehnicki dug Dokument

--- DocumentMsProject/DocumentMsProject/ServiceCalls/LoggerMicroservice.cs
+++ DocumentMsProject/DocumentMsProject/ServiceCalls/LoggerMicroservice.cs
@@ -22,7 +22,6 @@ namespace DocumentMsProject.ServiceCalls
         {
             using (HttpClient client = new HttpClient())
             {
-                var x = configuration["Services:LoggerMicroservice"];
                 Uri url = new Uri($"{ configuration["Services:LoggerMicroservice"] }api/logger");
 
                 HttpContent content = new StringContent(JsonConvert.SerializeObject(loggerDto));
